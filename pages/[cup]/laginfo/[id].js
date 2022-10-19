44fcfd4959938fd0dffeca9ddf98562f79b4b19e
Sebastian Svensson <sebastian.j.svensson@gmail.com>
2022-10-19 17:40:45 +0200

    massive changes to laginfo

--- pages/[cup]/laginfo/[id].js
+++ pages/[cup]/laginfo/[id].js
@@ -1,21 +1,30 @@
 /** @format */
 
 import KontaktInfo from '../../../components/LagInfoComponents/KontaktInfo';
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import classes from '../../../styles/LagInfo.module.css';
+import Klubbloggor from '../../../components/LagInfoComponents/Klubbloggor';
 
 export default function LagInfo(props) {
   return (
     <main className={classes.lagInfoContainer}>
       <header className={classes.headerLogo}>
-        <h1>{props.name}</h1>
+        <Klubbloggor />
+        <h1>{props.teamName}</h1>
       </header>
       <div className={classes.kontaktInfo}>
         <h2>Kontaktinfo</h2>
         <h2>{props.admin}</h2>
         <h2>{props.email}</h2>
       </div>
-      <div className={classes.spelarTrupp}>Spelartrupp</div>
+      <div className={classes.spelarTrupp}>
+        <h2>Spelartrupp</h2>
+        {props.players.map((player) => (
+          <div key={player.id}>
+            <h3>{player.name}</h3>
+          </div>
+        ))}
+      </div>
       <div className={classes.omkladningsRum}>Omklädningsrum</div>
       <div className={classes.hittaHit}>Hitta hit</div>
       <div className={classes.viktigInfo}>viktig info</div>
@@ -24,10 +33,21 @@ export default function LagInfo(props) {
 }
 
 export async function getServerSideProps({ params }) {
-  const raw = await fetch(`http://localhost:3000/api/teams/${params.id}`);
-  const res = await raw.json();
-
+  const [teamRes, playersRes] = await Promise.all([
+    fetch(`http://localhost:3000/api/teams/${params.id}`),
+    fetch(`http://localhost:3000/api/players/${params.id}`),
+  ]);
+  const [team, players] = await Promise.all([teamRes.json(), playersRes.json()]);
+  console.log(players);
+  console.log(team);
   return {
-    props: { id: 'id', email: res.email, name: res.name, abr: 'abr', admin: res.admin },
+    props: {
+      id: team.id,
+      email: team.email,
+      teamName: team.name,
+      abr: team.abr,
+      admin: team.admin,
+      players,
+    },
   };
 }

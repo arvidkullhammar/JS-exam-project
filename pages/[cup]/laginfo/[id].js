ad6bd249d02d041a4554d9c0b6fddeef664c7a3a
Sebastian Svensson <sebastian.j.svensson@gmail.com>
2022-10-19 10:24:10 +0200

    changes

--- pages/[cup]/laginfo/[id].js
+++ pages/[cup]/laginfo/[id].js
@@ -1,32 +1,15 @@
-import KontaktInfo from '../../../components/LagInfoComponents/KontaktInfo'
-import react, {useState, useEffect} from "react"
-
+import KontaktInfo from "../../../components/LagInfoComponents/KontaktInfo";
+import react, { useState, useEffect } from "react";
 
 export default function getServerSideProps(context) {
-
-  const [teamName, setTeamName] = useState("")
-
+  const [teamName, setTeamName] = useState("");
 
   useEffect(() => {
-
     fetch("http://localhost:3000/api/teams")
-    .then((rawData) => rawData.json())
-    .then((data) => console.log(data))
-    .catch((error) => console.log(error))
+      .then((rawData) => rawData.json())
+      .then((data) => console.log(data))
+      .catch((error) => console.log(error));
+  }, []);
 
-  }, []) 
-  
-
-
-
-  
-
-
-  return (
-    <div>{teamName}</div>
-  )
-    
-  
+  return <div>{teamName}</div>;
 }
-
-

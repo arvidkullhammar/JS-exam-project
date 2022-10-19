b3e3037bd696a9baa0bd8b409cba8f865df977fa
Sebastian Svensson <sebastian.j.svensson@gmail.com>
2022-10-19 10:50:22 +0200

    hejhej

--- pages/[cup]/laginfo/[id].js
+++ pages/[cup]/laginfo/[id].js
@@ -1,15 +1,29 @@
 import KontaktInfo from "../../../components/LagInfoComponents/KontaktInfo";
-import react, { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
+import classes from "../../../styles/LagInfo.module.css";
 
-export default function getServerSideProps(context) {
-  const [teamName, setTeamName] = useState("");
-
-  useEffect(() => {
-    fetch("http://localhost:3000/api/teams")
-      .then((rawData) => rawData.json())
-      .then((data) => console.log(data))
-      .catch((error) => console.log(error));
-  }, []);
-
-  return <div>{teamName}</div>;
+export default function LagInfo(props) {
+  return (
+    <main className={classes.lagInfoContainer}>
+      <header className={classes.headerLogo}></header>
+      <div className={classes.kontaktInfo}></div>
+      <div className={classes.spelarTrupp}></div>
+      <div className={classes.omkladningsRum}></div>
+      <div className={classes.hittaHit}></div>
+      <div className={classes.viktigInfo}></div>
+    </main>
+  );
 }
+
+// export async function getServerSideProps(context) {
+//   const [teamName, setTeamName] = useState(false);
+
+//   useEffect(() => {
+//     fetch("http://localhost:3000/api/teams")
+//       .then((rawData) => rawData.json())
+//       .then((data) => console.log(data))
+//       .catch((error) => console.log(error));
+//   }, []);
+
+//   return <div>{teamName}</div>;
+// }

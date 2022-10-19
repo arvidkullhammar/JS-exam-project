cd87b1aae9494867fdb40cc46019689ba9233d6e
Arvid <arvid.kullhammar@awacademy.training>
2022-10-19 09:47:51 +0200

    Added REST logic to API

--- pages/api/teams/index.js
+++ pages/api/teams/index.js
@@ -4,12 +4,15 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 export default async function handler(req, res) {
-  console.log(req.query.id);
-  const teams = await prisma.teams.findMany();
+  if (req.method !== "GET") {
+    res.status(405).json('Invalid request')
+  } else {
 
-  res.status(200).json(
-    teams.sort((a, b) => {
-      return a.id - b.id;
-    })
-  );
+    const teams = await prisma.teams.findMany();
+    res.status(200).json(
+      teams.sort((a, b) => {
+        return a.id - b.id;
+      })
+    );
+  }
 }

013909ff8a0a229469a9b42c126709e3ef8c9bd1
S.Olson <olsonseb@protonmail.com>
2022-10-20 10:44:54 +0200

    added Matchkorth-component

---
+++ components/Matchkort/Matchkort.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Matchkort() {
+  return (
+    <div>
+      <div>Mån 13 nov, 10:00</div>
+      <div>Infoknapp</div>
+      <div>Domsjö IF - Modo FF</div>
+      <div>P15</div>
+      <div>Kempevallen</div>
+      <div>Matchen pågår</div>
+      <div>13- 37</div>
+    </div>
+  )
+}
+export default Matchkort

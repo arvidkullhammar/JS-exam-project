5eae5a6fd9ea7e7566f1e5df7abb531e4090152b
Sebastian Svensson <sebastian.j.svensson@gmail.com>
2022-10-20 09:04:23 +0200

    added cupinfo

--- components/Cupinfo/Cupinfo.js
+++ components/Cupinfo/Cupinfo.js
@@ -1,6 +1,18 @@
-import React from 'react'
+/** @format */
+
+import React from 'react';
+import classes from '../Cupinfo/CupInfo.module.css';
 
 function Cupinfo() {
-  return <div>Cupinfo</div>
+  return (
+    <main className={classes.cupInfoContainer}>
+      <div className={classes.cupHeader}>Cup´header</div>
+      <div className={classes.allmant}>allmänt</div>
+      <div className={classes.oppeTider}>Öppetider</div>
+      <div className={classes.ovrigInfo}>övrig info</div>
+      <div className={classes.hittaHit}>Hitta hit</div>
+      <div className={classes.viktigInfo}>viktig info</div>
+    </main>
+  );
 }
-export default Cupinfo
+export default Cupinfo;

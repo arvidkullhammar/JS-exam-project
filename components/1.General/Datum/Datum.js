74fbf5d682c36450e71c2015dec3ee84e8886fb1
Sebastian Svensson <sebastian.j.svensson@gmail.com>
2022-10-21 15:47:02 +0200

    added dates

--- components/1.General/Datum/Datum.js
+++ components/1.General/Datum/Datum.js
@@ -1,7 +1,16 @@
 import React from "react";
 import classes from "./Datum.module.css";
+import moment from "moment";
+import { DateTime } from "luxon";
 
 function Datum() {
+  // write a functions that shows the coming 4 days from today and resets to index 0 after the end of the array
+
+  let day = DateTime.now().toFormat("cccc dd/MM ");
+  let tomorrow = DateTime.now().plus({ days: 1 }).toFormat("cccc dd/MM ");
+  let dayAftertomorrow = DateTime.now().plus({ days: 2 }).toFormat("cccc dd/MM ");
+  let dayAfterDayAftertomorrow = DateTime.now().plus({ days: 3 }).toFormat("cccc dd/MM ");
+
   return (
     <div className={classes.circleContainer}>
       <div className={classes.circle}>
@@ -11,26 +20,26 @@ function Datum() {
       </div>
       <div className={classes.circle}>
         <div className={classes.textWrap}>
-          <span>Dag</span>
-          <span>Datum</span>
+          <span>{day}</span>
+          <span></span>
         </div>
       </div>
       <div className={classes.circle}>
         <div className={classes.textWrap}>
-          <span>Dag</span>
-          <span>Datum</span>
+          <span>{tomorrow}</span>
+          <span></span>
         </div>
       </div>
       <div className={classes.circle}>
         <div className={classes.textWrap}>
-          <span>Dag</span>
-          <span>Datum</span>
+          <span>{dayAftertomorrow}</span>
+          <span></span>
         </div>
       </div>
       <div className={classes.circle}>
         <div className={classes.textWrap}>
-          <span>Dag</span>
-          <span>Datum</span>
+          <span>{dayAfterDayAftertomorrow}</span>
+          <span></span>
         </div>
       </div>
     </div>

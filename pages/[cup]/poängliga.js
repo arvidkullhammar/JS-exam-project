c4d37e805232c239d167fd9d16e4dc76bbb8e34f
S.Olson <olsonseb@protonmail.com>
2022-10-21 11:01:10 +0200

    fixed routing

--- pages/[cup]/poängliga.js
+++ pages/[cup]/poängliga.js
@@ -1,11 +1,11 @@
 /** @format */
 import React from 'react'
-import Matchkort from '../../components/Matchkort/Matchkort'
 import ToggleBetween from '../../components/1.General/ToggleBetween/ToggleBetween'
 import Datum from '../../components/1.General/Datum/Datum'
 import Image from 'next/future/image'
 import leksandH from '../.././Images/leksandhockeyheader.png'
 import classes from './spelschema.module.css'
+import Poängliga from '../../components/Poängliga/Poängliga'
 
 export default function Spelschema(props) {
   return (
@@ -13,9 +13,7 @@ export default function Spelschema(props) {
       <Image className={classes.heroImg} src={leksandH} alt="photo of leksand logo" width="50px" height="50px" />
       <Datum />
       <ToggleBetween />
-      <Matchkort />
-      <Matchkort />
-      <Matchkort />
+      <Poängliga />
     </>
   )
 }
--- pages/[cup]/tabell.js
+++ pages/[cup]/tabell.js
@@ -1,11 +1,11 @@
 /** @format */
 import React from 'react'
-import Matchkort from '../../components/Matchkort/Matchkort'
 import ToggleBetween from '../../components/1.General/ToggleBetween/ToggleBetween'
 import Datum from '../../components/1.General/Datum/Datum'
 import Image from 'next/future/image'
 import leksandH from '../.././Images/leksandhockeyheader.png'
 import classes from './spelschema.module.css'
+import Tabell from '../../components/Tabell/Tabell'
 
 export default function Spelschema(props) {
   return (
@@ -13,9 +13,7 @@ export default function Spelschema(props) {
       <Image className={classes.heroImg} src={leksandH} alt="photo of leksand logo" width="50px" height="50px" />
       <Datum />
       <ToggleBetween />
-      <Matchkort />
-      <Matchkort />
-      <Matchkort />
+      <Tabell />
     </>
   )
 }

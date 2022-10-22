7aef1f90085861f277a2f0fc4c58c3757ca66e21
Sebastian Svensson <sebastian.j.svensson@gmail.com>
2022-10-22 09:43:28 +0200

    .

--- components/1.General/Datum/Datum.js
+++ components/1.General/Datum/Datum.js
@@ -1,21 +1,58 @@
-import React from "react";
-import classes from "./Datum.module.css";
-import moment from "moment";
-import { DateTime } from "luxon";
+/** @format */
+
+import React from 'react';
+import classes from './Datum.module.css';
+import moment from 'moment';
+import { DateTime } from 'luxon';
 
 function Datum() {
   // write a functions that shows the coming 4 days from today and resets to index 0 after the end of the array
 
-  let day = DateTime.now().toFormat("cccc dd/MM ");
-  let tomorrow = DateTime.now().plus({ days: 1 }).toFormat("cccc dd/MM ");
-  let dayAftertomorrow = DateTime.now().plus({ days: 2 }).toFormat("cccc dd/MM ");
-  let dayAfterDayAftertomorrow = DateTime.now().plus({ days: 3 }).toFormat("cccc dd/MM ");
+  let day = DateTime.now()
+    .toFormat('cccc dd/MM ')
+    .replace('Saturday', 'Lör')
+    .replace('Sunday', 'Sön')
+    .replace('Monday', 'Mån')
+    .replace('Tuesday', 'Tis')
+    .replace('Wednesday', 'Ons')
+    .replace('Thursday', 'Tor')
+    .replace('Friday', 'Fre');
+  let tomorrow = DateTime.now()
+    .plus({ days: 1 })
+    .toFormat('cccc dd/MM ')
+    .replace('Saturday', 'Lör')
+    .replace('Sunday', 'Sön')
+    .replace('Monday', 'Mån')
+    .replace('Tuesday', 'Tis')
+    .replace('Wednesday', 'Ons')
+    .replace('Thursday', 'Tor')
+    .replace('Friday', 'Fre');
+  let dayAftertomorrow = DateTime.now()
+    .plus({ days: 2 })
+    .toFormat('cccc dd/MM ')
+    .replace('Saturday', 'Lör')
+    .replace('Sunday', 'Sön')
+    .replace('Monday', 'Mån')
+    .replace('Tuesday', 'Tis')
+    .replace('Wednesday', 'Ons')
+    .replace('Thursday', 'Tor')
+    .replace('Friday', 'Fre');
+  let dayAfterDayAftertomorrow = DateTime.now()
+    .plus({ days: 3 })
+    .toFormat('cccc dd/MM ')
+    .replace('Saturday', 'Lör')
+    .replace('Sunday', 'Sön')
+    .replace('Monday', 'Mån')
+    .replace('Tuesday', 'Tis')
+    .replace('Wednesday', 'Ons')
+    .replace('Thursday', 'Tor')
+    .replace('Friday', 'Fre');
 
   return (
     <div className={classes.circleContainer}>
       <div className={classes.circle}>
         <div className={classes.textWrap}>
-          <span>All</span>
+          <span>Alla</span>
         </div>
       </div>
       <div className={classes.circle}>

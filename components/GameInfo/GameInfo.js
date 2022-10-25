f684ddc65295d0bfab1964c20b3c396849f36379
Sebastian Svensson <sebastian.j.svensson@gmail.com>
2022-10-25 16:41:25 +0200

    saker som inte funkar

--- components/GameInfo/GameInfo.js
+++ components/GameInfo/GameInfo.js
@@ -51,35 +51,5 @@ function GameInfo(props) {
     </div>
   );
 }
-export default GameInfo;
 
-{
-  /* <div>
-<table className={classes.tabellStyling}>
-  <thead>
-    <tr>
-      <th className={classes.textCenter}></th>
-      <th className={classes.textCenter}>Matchstatistik</th>
-      <th className={classes.textCenter}></th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <td className={classes.textLeft}>0</td>
-      <td>Mål</td>
-      <td>0</td>
-    </tr>
-    <tr>
-      <td className={classes.textLeft}>0</td>
-      <td>Gula kort</td>
-      <td>0</td>
-    </tr>
-    <tr>
-      <td className={classes.textLeft}>0</td>
-      <td>Röda kort</td>
-      <td>0</td>
-    </tr>
-  </tbody>
-</table>
-</div> */
-}
+export default GameInfo;

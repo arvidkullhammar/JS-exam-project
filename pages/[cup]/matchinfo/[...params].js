9de433917ef8ef8be6215bb09db94f6ee73ae39b
Sebastian Svensson <sebastian.j.svensson@gmail.com>
2022-10-25 16:41:46 +0200

    .

--- pages/[cup]/matchinfo/[...params].js
+++ pages/[cup]/matchinfo/[...params].js
@@ -1,21 +1,21 @@
-import React from 'react'
-import GameInfo from '../../../components/GameInfo/GameInfo'
-import { useRouter } from 'next/router'
+import React from "react";
+import GameInfo from "../../../components/GameInfo/GameInfo";
+import { useRouter } from "next/router";
 
 export default function gameInfoPages(props) {
-  const teamOneId = props.params.params[0]
-  const teamTwoId = props.params.params[1]
-  return (
-    <div>
-      <GameInfo teamOne={teamOneId} teamTwo={teamTwoId} />{' '}
-    </div>
-  )
+  const teamOneId = props.params.params[0];
+  const teamTwoId = props.params.params[1];
+  console.log(props);
+  return <div>{<GameInfo teamOne={teamOneId} teamTwo={teamTwoId} />}</div>;
 }
 
-export async function getServerSideProps({ params }) {
+export async function getServerSideProps(context) {
+  const response = await fetch(`http://localhost:3000/api/teams/`);
+  const data = await response.json();
+
   return {
     props: {
-      params,
+      teamId: data,
     },
-  }
+  };
 }

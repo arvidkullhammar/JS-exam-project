f9fc0f0b4a9bd86da6b3f28dc915c6dc5052ae3a
Sebastian Svensson <sebastian.j.svensson@gmail.com>
2022-10-26 11:31:14 +0200

    Co-authored-by: Niclas Pahlfer <Niclas@pahlfer.com>

--- components/TeamInfoComponents/TeamLogos.js
+++ components/TeamInfoComponents/TeamLogos.js
@@ -2,12 +2,17 @@
 
 import React from 'react';
 import Image from 'next/future/image';
-import aik from '../../public/images/mocklogos/klubb_aik.png';
-import kik from '../../public/images/mocklogos/kik_logga.png';
-import almtuna from '../../public/images/mocklogos/klubb_almtuna.png';
-import bik from '../../public/images/mocklogos/klubb_bik.png';
-import loven from '../../public/images/mocklogos/klubb_loven.png';
-import modo from '../../public/images/mocklogos/mini_modo.png';
+import aik from 'public/images/mocklogos/klubb_aik.png';
+import kik from 'public/images/mocklogos/kik_logga.png';
+import almtuna from 'public/images/mocklogos/klubb_almtuna.png';
+import bik from 'public/images/mocklogos/klubb_bik.png';
+import loven from 'public/images/mocklogos/klubb_loven.png';
+import modo from 'public/images/mocklogos/mini_modo.png';
+import { prominent } from 'color.js';
+
+prominent(aik, { amount: 2 }).then((color) => {
+  console.log(color); // [241, 221, 63]
+});
 
 export default function Klubbloggor() {
   const randomLogo = () => {

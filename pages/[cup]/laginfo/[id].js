8886f42637170c741e102497a0e9ae1156270e32
Arvid <arvid.kullhammar@awacademy.training>
2022-10-26 13:28:26 +0200

    removed console.logs

--- pages/[cup]/laginfo/[id].js
+++ pages/[cup]/laginfo/[id].js
@@ -14,7 +14,6 @@ export default function TeamInfo(props) {
   //Update roster when player is added
   const [playerArr, setPlayerArr] = useState(props.players);
   useEffect(() => {
-    console.log('updated player roster');
   }, [playerArr]);
   const addCallback = (obj) => {
     let newPlayerArr = [...playerArr];
@@ -27,18 +26,14 @@ export default function TeamInfo(props) {
     console.log('removeid', removeId)
     let newPlayerArr = [...playerArr];
     let removePlayerIndex = newPlayerArr.findIndex((obj) => obj.id === removeId);
-    console.log(removePlayerIndex, 'remove index')
 
     if (removePlayerIndex > -1) {
-      console.log('if run')
       newPlayerArr.splice(removePlayerIndex, 1);
     }
 
     setPlayerArr(newPlayerArr);
-    console.log('newplayerarr',newPlayerArr)
   };
 
-  console.log('colors', colors);
 
   return (
     <main

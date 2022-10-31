23bdefbfd8040b823aabb54389e13fa9bcfc14b0
Arvid Kullhammar <91427561+arvidkullhammar@users.noreply.github.com>
2022-10-31 11:12:58 +0100

    uploaded exams project to new repo

--- pages/api/players/delete.js
+++ pages/api/players/delete.js
@@ -1,7 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import { PrismaClient } from '@prisma/client'
-
-const prisma = new PrismaClient()
+import { prisma } from '../../../db'
 
 export default async function handler(req, res) {
   if (req.method !== "DELETE") {
